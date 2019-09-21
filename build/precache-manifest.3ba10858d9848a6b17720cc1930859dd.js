96f2b74c2f8bd35cde36821e9cc2bfd6b0424b39
nuri engin <uzakkultur@gmail.com>
2019-09-22 00:57:32 +0300

    Robos has been builded

---
+++ build/precache-manifest.3ba10858d9848a6b17720cc1930859dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134183268420e656d12236d8e80b0b52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f4d5c8add7d67453a7e",
+    "url": "/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "5aee3a5f449e73cd9e0a",
+    "url": "/static/css/main.0fa9c80e.chunk.css"
+  },
+  {
+    "revision": "5f4d5c8add7d67453a7e",
+    "url": "/static/js/2.4a265b5a.chunk.js"
+  },
+  {
+    "revision": "5aee3a5f449e73cd9e0a",
+    "url": "/static/js/main.4503f231.chunk.js"
+  },
+  {
+    "revision": "0ca0515cb697c48605ca",
+    "url": "/static/js/runtime-main.3e7791bc.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  }
+]);
